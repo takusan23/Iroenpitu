37e037d81c536c74024840584dded1fb23975919
takusan23 <takumu1200@gmail.com>
2025-05-11 02:10:17 +0900

    設定をスクロール

--- composeApp/src/commonMain/kotlin/io/github/takusan23/iroenpitu/ui/SettingBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/io/github/takusan23/iroenpitu/ui/SettingBottomSheet.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -37,7 +39,11 @@ fun SettingBottomSheet(onDismissRequest: () -> Unit) {
     }
 
     ModalBottomSheet(onDismissRequest = onDismissRequest) {
-        Column(modifier = Modifier.padding(10.dp)) {
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
             Row {
                 Text(
                     text = "初期設定",
